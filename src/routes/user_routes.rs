244912e102441d661c504d5a93858532f8666584
Kevin Ngo <quocdatngo2003@gmail.com>
2026-01-28 11:41:33 +1100

    Specify more routes for different CRUD operations

--- src/routes/user_routes.rs
+++ src/routes/user_routes.rs
@@ -1,10 +1,12 @@
 use actix_web::web;
-use crate::controllers::user_controllers::{create_user, get_user};
+use crate::controllers::user_controllers::{create_user, get_all_users, get_user, update_user};
 
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(
 web::scope("/users")
+            .route("", web::get().to(get_all_users))
             .route("", web::post().to(create_user))
             .route("/{id}", web::get().to(get_user))
+            .route("/{id}", web::patch().to(update_user))
     );
 }
