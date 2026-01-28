0fbc1c6ee120b2633f9a2afedfd094b4df0f45ff
Kevin Ngo <quocdatngo2003@gmail.com>
2026-01-28 11:41:58 +1100

    Finalse the controller functions for user

--- src/controllers/user_controllers.rs
+++ src/controllers/user_controllers.rs
@@ -1,24 +1,72 @@
 use actix_web::{HttpResponse, web};
-use crate::models::user::{CreateUser, User};
+use crate::models::user::{NewUser, UpdateUser, User};
+use crate::db::connection::establish_connection;
+use diesel::prelude::*;
 
-pub async fn create_user(user: web::Json<CreateUser>) -> HttpResponse {
-    // Placeholder
-    let new_user = User {
-        id: 1,
-        email: user.email.clone(),
-        password: user.password.clone(), // TODO: Hash the password the save it to database
+pub async fn create_user(body: web::Json<NewUser>) -> HttpResponse {
+    println!("Creating new user: {:?}", body);
+    use crate::schema::users::dsl::*;
+
+    let mut connection = establish_connection();
+
+    let new_user = NewUser {
+        email: body.email.clone(),
+        password: body.password.clone(),
     };
+
+    diesel::insert_into(users)
+        .values(&new_user)
+        .execute(&mut connection)
+        .expect("Error saving new user");
 
     HttpResponse::Created().json(new_user)
 }
 
-pub async fn get_user(id: web::Path<u32>) -> HttpResponse {
-    // Mock user for now
-    let mock_user = User {
-        id: 1,
-        email: String::from("quocdatngo2003@gmail.com"),
-        password: String::from("dat123"),
+pub async fn update_user(id: web::Path<i32>, body: web::Json<UpdateUser>) -> HttpResponse {
+    println!("Update user with id: {:?}", id);
+    use crate::schema::users::dsl::*;
+    
+    let id = id.into_inner();
+    let mut connection = establish_connection();
+
+    let update = UpdateUser {
+        email: body.email.clone(),
+        password: body.password.clone(),
     };
 
-    HttpResponse::Ok().json(mock_user)
+    diesel::update(users.find(id))
+        .set(&update)
+        .execute(&mut connection)
+        .expect("Error updating user");
+
+    HttpResponse::Ok().json(update)
+}
+
+pub async fn get_all_users() -> HttpResponse {
+    println!("Showing all users");
+    use crate::schema::users::dsl::*;
+
+    let mut connection = establish_connection();
+
+    let users_list: Vec<User> = users
+        .load::<User>(&mut connection)
+        .expect("Error loading users");
+
+    HttpResponse::Ok().json(users_list)
+}
+
+pub async fn get_user(id: web::Path<i32>) -> HttpResponse {
+    println!("Showing user with given id {:?}", id);
+    
+    use crate::schema::users::dsl::*;
+    
+    let id = id.into_inner();
+    let mut connection = establish_connection();
+
+    let target_user: User = users
+        .find(id)
+        .first(&mut connection)
+        .expect("Error retrieving user with given id {user_id}");
+
+    HttpResponse::Ok().json(target_user)
 }
