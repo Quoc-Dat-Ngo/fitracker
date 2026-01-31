75338ee9f2566de8a925608834e5a4b7de083065
Kevin Ngo <quocdatngo2003@gmail.com>
2026-01-31 16:23:35 +1100

    Add endpoint for root route for sanity checking; Add a scope /api/v1 for /users; Import services, repositories and errors modules

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,23 @@ mod controllers;
 mod models;
 mod db;
 mod schema;
+mod services;
+mod repositories;
+mod errors;
 
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpResponse, HttpServer, web};
 use crate::routes::user_routes::init;
 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let port = 8080;
+    let port = 3030;
     println!("Starting sever on port {port}");
 
     HttpServer::new(|| {
         App::new()
-            .configure(init)
+            .service(web::scope("/api/v1").configure(init))
+            .route("/", web::get().to(|| async {HttpResponse::Ok().json("Server is alive, hooray!")}))
     })
         .bind(("127.0.0.1", port))?
         .run()
