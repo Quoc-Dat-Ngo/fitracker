4ec57b8edee88e690b106a4a953cd0010bfc3cc6
Kevin Ngo <quocdatngo2003@gmail.com>
2026-01-31 16:27:29 +1100

    Rewrite routings to use resource to group same enpoints

--- src/routes/user_routes.rs
+++ src/routes/user_routes.rs
@@ -1,12 +1,18 @@
 use actix_web::web;
-use crate::controllers::user_controllers::{create_user, get_all_users, get_user, update_user};
+use crate::controllers::user_controllers::{create_user_controller, get_all_users, get_user, update_user};
 
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(
 web::scope("/users")
-            .route("", web::get().to(get_all_users))
-            .route("", web::post().to(create_user))
-            .route("/{id}", web::get().to(get_user))
-            .route("/{id}", web::patch().to(update_user))
+            .service(
+        web::resource("")
+                    .get(get_all_users)
+                    .post(create_user_controller)
+            )
+            .service(
+        web::resource("/{id}")
+                    .get(get_user)
+                    .patch(update_user)
+            )
     );
 }
