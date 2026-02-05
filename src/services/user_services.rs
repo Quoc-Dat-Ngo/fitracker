47eeaa7227c5b6f587f621e6a5e96361f6913aad
Kevin Ngo <quocdatngo2003@gmail.com>
2026-02-05 12:58:59 +1100

    Utilise web::block to carry out asynchronous query to the database optimise run time for retrieving on /users

--- src/services/user_services.rs
+++ src/services/user_services.rs
@@ -1,10 +1,38 @@
-use crate::{errors::AppError, models::user::{NewUser, User}, repositories::user_repositories::UserRepository};
+use actix_web::web;
 
+use crate::{
+    db::pool::DbPool,
+    dtos::CreateUserRequest,
+    errors::AppError,
+    models::user::{NewUser, User},
+    repositories::user_repositories::UserRepository,
+};
 
+pub async fn create_user(
+    body: CreateUserRequest,
+    pool: web::Data<DbPool>,
+    repo: web::Data<dyn UserRepository>,
+) -> Result<User, AppError> {
+    // Clone the pointers (cheap)
+    let pool = pool.clone();
+    let repo = repo.clone();
 
-pub async fn create_user(body: NewUser) -> Result<User, AppError> {
-    // TODO: Validation + Hashing password
+    // We use ? here because web::block returns Result<..., BlockingError>
+    // and we now have an impl From<BlockingError> for AppError
+    web::block(move || -> Result<User, AppError> {
+        let mut conn = pool
+            .get()
+            .map_err(|e| AppError::DatabaseError(e.to_string()))?;
 
+        let new_user = NewUser {
+            email: body.email,
+            password: body.password, // TODO use argon2 to hash the password
+        };
 
-    UserRepository::create(&body)
+        // This returns Result<User, diesel::result::Error>
+        // The ? operator converts diesel error to AppError using our impl From
+        repo.create(new_user, &mut conn).map_err(AppError::from)
+    })
+    .await?
+    // .map_err(|e| e)
 }
