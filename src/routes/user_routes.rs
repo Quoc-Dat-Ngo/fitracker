476a56bb35c023ce20bcf3200d8d57a606753d9e
Kevin Ngo <quocdatngo2003@gmail.com>
2026-02-05 12:56:13 +1100

    Refactor to use web::resource to group similar endpoints

--- src/routes/user_routes.rs
+++ src/routes/user_routes.rs
@@ -1,18 +1,16 @@
+use crate::controllers::user_controllers::{
+    create_user_controller, get_all_users, get_user, update_user,
+};
 use actix_web::web;
-use crate::controllers::user_controllers::{create_user_controller, get_all_users, get_user, update_user};
 
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(
-web::scope("/users")
+        web::scope("/users")
             .service(
-        web::resource("")
+                web::resource("")
                     .get(get_all_users)
-                    .post(create_user_controller)
+                    .post(create_user_controller),
             )
-            .service(
-        web::resource("/{id}")
-                    .get(get_user)
-                    .patch(update_user)
-            )
+            .service(web::resource("/{id}").get(get_user).patch(update_user)),
     );
 }
